f15d1ba382485f5914ca0da333aa864226792ddb
Eric Morris <elm232@cornell.edu>
2014-08-04 21:08:01 -0700

    Collect all params

--- lib/image.rb
+++ lib/image.rb
@@ -2,7 +2,7 @@ require 'rmagick'
 include Magick
 
 def generate_image width, height
-  img = Image.new(width.to_int, height.to_int) { self.background_color = "red" }
+  img = Image.new(width.to_i, height.to_i) { self.background_color = "red" }
   img.format = 'jpg'
   img.to_blob
 end
--- lib/wabbit.rb
+++ lib/wabbit.rb
@@ -6,9 +6,10 @@ configure {
 }
  
 class Pumatra < Sinatra::Base
-  get '/:width/:height' do
-    content_type 'image/jpg'
-    generate_image params[:width], params[:height]
+  get '/:width/:height.?:format?/?:color?.?:format?/?:image_id?.?:format?' do
+    return params.inspect
+    # content_type 'image/jpg'
+    # generate_image params[:width], params[:height]
   end
  
   run! if app_file == $0
