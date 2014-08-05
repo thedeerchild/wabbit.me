5e9553535c0bc519ccf3a01eea84e96c48f44606
Eric Morris <elm232@cornell.edu>
2014-08-04 21:15:20 -0700

    Return image of right dimensions and format

--- lib/image.rb
+++ lib/image.rb
@@ -1,8 +1,8 @@
 require 'rmagick'
 include Magick
 
-def generate_image width, height
+def generate_image width, height, format
   img = Image.new(width.to_i, height.to_i) { self.background_color = "red" }
-  img.format = 'jpg'
+  img.format = format
   img.to_blob
 end
--- lib/wabbit.rb
+++ lib/wabbit.rb
@@ -7,9 +7,13 @@ configure {
  
 class Pumatra < Sinatra::Base
   get '/:width/:height.?:format?/?:color?.?:format?/?:image_id?.?:format?' do
-    return params.inspect
-    # content_type 'image/jpg'
-    # generate_image params[:width], params[:height]
+    format = if (params[:format] && %w(jpg jpeg png gif).include?(params[:format]))
+      params[:format]
+    else
+      'jpg'
+    end
+    content_type "image/#{format}"
+    generate_image params[:width], params[:height], format
   end
  
   run! if app_file == $0
