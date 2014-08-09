6e39e85427755e28b662ce998b4ccb7064c9e977
Eric Morris <elm232@cornell.edu>
2014-08-08 21:02:33 -0700

    Fix Puma config paths

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,10 @@
-root = "#{Dir.getwd}"
+current_path = "/home/deployer/wabbit.me/current"
+shared_path = "/home/deployer/wabbit.me/shared"
  
-bind "unix://#{root}/tmp/puma/socket"
-pidfile "#{root}/tmp/puma/pid"
-state_path "#{root}/tmp/puma/state"
-rackup "#{root}/config.ru"
+bind "unix://#{shared_path}/sockets/puma.sock"
+pidfile "#{shared_path}/tmp/puma/pid"
+state_path "#{shared_path}/tmp/puma/state"
+rackup "#{current_path}/config.ru"
  
 threads 4, 8
  
